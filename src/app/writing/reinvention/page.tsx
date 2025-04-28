724ebc1557936eb3754276cbc90c6d6ae8f6d999
Ayah <ayahalmusaddy2@gmail.com>
2025-04-28 18:09:11 -0500

    writing piece updated

--- src/app/writing/reinvention/page.tsx
+++ src/app/writing/reinvention/page.tsx
@@ -2,18 +2,23 @@ export default function ReinventionPost() {
   return (
     <section className="space-y-5 flex justify-center">
       <div className="max-w-prose w-full space-y-5">
-        <h1 className="text-2xl font-medium text-gray-800">What constitues reinvention?</h1>
+        <h1 className="text-2xl font-medium text-gray-800">What constitutes reinvention?</h1>
         <p>{`The notion of reinventing yourself is a popular phenomenon amongst our capitalistic society. How to become better. How to glow up. How to make more money. Here’s what these millionaires aren’t telling you.  15 ways to completely change your life.`}</p>
 
-        <p>{`It’s almost debilitating, wondering how many things you can alter about your reality. Looking at yourself in the mirror and not seeing the person you want to see. Unhappiness is an unfortunate side effect from a society that prioritizes the individual over the community. Illusion of self care. Striving to optimize your productivity. Or shall I say, your self worth. No apparent distinction between the two. `}</p>
+        <p>{`It’s almost debilitating, wondering how many things you can alter about your reality. Looking at yourself in the mirror and not seeing the person you want to see. Unhappiness is an unfortunate side effect of a society that prioritizes the individual over the community. Illusion of self-care. Striving to optimize your productivity. Or shall I say, your self-worth. No apparent distinction between the two. `}</p>
 
-        <p>{`But how does one become the “best version of themselves” when we cannot clearly define what makes someone “the best”. Are the richest people the standard? The prettiest? The most politically active? The ones with the loving families? In the year 2025, what is the American dream? And is the American dream something we should even account for when America is one of the most ethically questionable countries to exist. Do we look towards religion for the standard of a good life? For the standards of being a good human being? `}</p>
+        <p>{`But how does one become the “best version of themselves” when we cannot clearly define what makes someone “the best”? Are the wealthiest people the standard? The prettiest? The most politically active? The ones with the loving families? In the year 2025, what is the American dream? And is the American dream something we should even account for when America is one of the most ethically questionable countries to exist? Do we look towards religion for the standard of a good life? For the standards of being a good human being?`}</p>
 
         <p>{`Reinvention begins with understanding who you are. Tracking your current habits and routines. Trailing your thoughts. Dissecting your core beliefs. Reflecting on your childhood. A person cannot change their entire being without understanding who they’re working with. `} </p>
 
-        <p>{`Then, figuring out who you want to become and why.  `}</p>
+        <p>Then, figuring out <i>who</i> you want to become and <i>why.</i></p>
         
-        <p>{`The how is, arguably, the most difficult part. It takes altering your daily decisions. Actively making different choices your mind is not used to making. Questioning every decision takes a lot of energy. It can be difficult to maintain if you try to rewire too much of your brain at once. But if done correctly, if a person pushes through, changing oneself becomes addicting. There is nothing more powerful than knowing you have the ability to transform every part of yourself. A chameleon of personhood. To be thrown into any situation and have the utmost reassurance in your ability to survive. I would argue, the ability to reinvent is the strongest skill a human being could develop. `}</p>
+        <p>The <i>how</i> is, arguably, the most difficult part. It takes altering your daily decisions. Actively making different choices your mind is not used to making. Questioning every decision takes a lot of energy. It can be difficult to maintain if you try to rewire too much of your brain at once. But if done correctly, if a person pushes through, changing oneself becomes addictive. There is nothing more powerful than knowing you can transform every part of yourself. A chameleon of personhood. To be thrown into any situation and have the utmost reassurance in your ability to survive. I would argue, the ability to reinvent is the strongest skill a human being could develop.</p>
+
+        <p>{'Now, the obsession with reinvention is where the problem lies. Setting goals you can never reach. The act of running a marathon and searching for the next race, never satisfied with your time. It’s not so much the desire to grow as it is the dissatisfaction of never being enough. '}</p>
+
+        <p>{'There is a fragile line between healthy growth, owning up to who you are, and refusing happiness with yourself, yearning to become someone else. Reinvention is a powerful tool - it can reignite your desire to live, reestablish self-worth, and spark passion and desire in a person’s life. But as you break down and build up the person you want to be, do not get too hooked on the feeling, and beware of the adrenaline rush it gives. Once you reinvent yourself the first time, it can be hard not to want to do it again. And again. And again. Until you stop one day and wonder, Who am I? And why?'}</p>
+
       </div>
     </section>
   );
