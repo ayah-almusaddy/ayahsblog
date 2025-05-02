c3fccb76661cde5d919e44b14bd38ad457da40a0
Ayah <ayahalmusaddy2@gmail.com>
2025-05-02 12:02:00 -0500

    qotd

--- src/app/components/quote.tsx
+++ src/app/components/quote.tsx
@@ -3,9 +3,9 @@ export default function Quote() {
     <section className="text-center py-12">
       <h1 className="text-3xl font-bold text-gray-800">quote of the day</h1>
       <blockquote className="mt-6 text-xl italic text-gray-800">
-        {`"There is nothing I would not do for those who are really my friends. I have no notion of loving people by halves, it is not my nature. "`}
+        {`"If you hear a voice within you say ‘you cannot paint’, then by all means paint and that voice will be silenced. "`}
       </blockquote>
-      <footer className="mt-2 text-gray-600">- Jane Austen</footer>
+      <footer className="mt-2 text-gray-600">- Vincent Van Gogh</footer>
     </section>
   );
 }
