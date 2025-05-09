c7e45d540fe9119d8e8bd90ab28f32e6981f8447
Ayah <ayahalmusaddy2@gmail.com>
2025-05-08 19:46:10 -0500

    qotd

--- src/app/components/quote.tsx
+++ src/app/components/quote.tsx
@@ -3,9 +3,9 @@ export default function Quote() {
     <section className="text-center py-12">
       <h1 className="text-3xl font-bold text-gray-800">quote of the day</h1>
       <blockquote className="mt-6 text-xl italic text-gray-800">
-        {`"If you hear a voice within you say ‘you cannot paint’, then by all means paint and that voice will be silenced. "`}
+        {`" Spring is the time of plans and projects. "`}
       </blockquote>
-      <footer className="mt-2 text-gray-600">- Vincent Van Gogh</footer>
+      <footer className="mt-2 text-gray-600">- Leo Tolstoy</footer>
     </section>
   );
 }
