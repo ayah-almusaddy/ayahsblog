96711c45f5c042cadebca0cded048fad9df18b9b
Ayah <ayahalmusaddy2@gmail.com>
2025-05-09 19:07:31 -0500

    qotd

--- src/app/components/quote.tsx
+++ src/app/components/quote.tsx
@@ -3,9 +3,9 @@ export default function Quote() {
     <section className="text-center py-12">
       <h1 className="text-3xl font-bold text-gray-800">quote of the day</h1>
       <blockquote className="mt-6 text-xl italic text-gray-800">
-        {`" Spring is the time of plans and projects. "`}
+        {`" One must finish something to start something."`}
       </blockquote>
-      <footer className="mt-2 text-gray-600">- Leo Tolstoy</footer>
+      <footer className="mt-2 text-gray-600">- Tamerlan Kuzgov</footer>
     </section>
   );
 }
