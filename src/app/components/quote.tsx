f4b45e4f4f28171664f9a4f7bc2199cd800b41d8
Ayah <ayahalmusaddy2@gmail.com>
2025-05-28 16:34:56 -0500

    qotd

--- src/app/components/quote.tsx
+++ src/app/components/quote.tsx
@@ -3,9 +3,9 @@ export default function Quote() {
     <section className="text-center py-12">
       <h1 className="text-3xl font-bold text-gray-800">quote of the day</h1>
       <blockquote className="mt-6 text-xl italic text-gray-800">
-        {`" One must finish something to start something."`}
+        {`" It hurts to want it all, so many things that can't coexist within the same life."`}
       </blockquote>
-      <footer className="mt-2 text-gray-600">- Tamerlan Kuzgov</footer>
+      <footer className="mt-2 text-gray-600">- Emily Henry</footer>
     </section>
   );
 }
