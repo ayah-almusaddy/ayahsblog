18f54cf5dea0cb1bd97d9e1c0ac9352780314fee
Ayah <ayahalmusaddy2@gmail.com>
2025-11-01 21:35:23 -0500

    changed about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,14 @@ export default function AboutPage() {
 		<h1 className="text-3xl font-semibold text-gray-800">about me</h1>
 		<h2 className="text-xl font-medium">Who am I?</h2>
 	
-		<p>{`My name is Ayah. I’m 21, and in my last semester of my undergraduate degree, studying Computer Science. I read a lot, write a lot, and think too much. What better way to utilize my long and winding musings than to start a blog? Stay and read a bit if you’re interested. Or don’t. I don’t particularly care.`}</p>
+		<p>{`I'll write this later.`}</p>
+
+		<h2 className="text-xl font-medium">Credits</h2>
+
+		<p>{`The home page was beautifully designed by the genius, spectacular Abigail Jensen.`}</p>
+
+		<p>{`All writing is done by me, unless quoted or cited. I am disgusted by AI replacing creativity, so unless I'm doing some strange social experiment, nothing will be written with AI (except code of the website).`}</p>
+
 		</div>
 		</section>
 	);
